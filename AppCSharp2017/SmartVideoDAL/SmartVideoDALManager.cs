2fe4c1eeffd59ea3e4f2d7b23f58764e70e1f655
Morghen <remy.mauhin@gmail.com>
2017-11-23 15:37:40 +0100

    modif DAL

---
+++ AppCSharp2017/SmartVideoDAL/SmartVideoDALManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartVideoDTOLibrary;
+
+namespace SmartVideoDAL
+{
+    public class SmartVideoDALManager
+    {
+        SmartVideoDBManagementDataContext dc = null;
+
+        public SmartVideoDALManager()
+        {
+            if(dc == null)
+            {
+                dc = new SmartVideoDBManagementDataContext();
+            }
+        }
+
+        public bool addHit(int id, TypeEnum type, DateTime dt)
+        {
+            return false;
+        }
+    }
+}
--- AppCSharp2017/SmartVideoDTOLibrary/Class1.cs
+++ AppCSharp2017/SmartVideoDTOLibrary/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartVideoDAL
+namespace SmartVideoDTOLibrary
 {
     public class Class1
     {
