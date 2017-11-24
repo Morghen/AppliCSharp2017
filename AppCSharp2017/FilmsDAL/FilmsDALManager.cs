04b4109d643dfb5317edbd2f616c57e617458840
Morghen <remy.mauhin@gmail.com>
2017-11-24 09:15:51 +0100

    filmdb

---
+++ AppCSharp2017/FilmsDAL/FilmsDALManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmsDAL
+{
+    public class FilmsDALManager
+    {
+        public FilmsDBManagementDataContext dc = null;
+        public List<String> lf = null;
+
+        public FilmsDALManager()
+        {
+            if(dc == null)
+            {
+                dc = new FilmsDBManagementDataContext();
+            }
+
+            else
+            {
+                String constr = "DataSource = " + servername + " ; Initial Catalog = FilmDB; Integrated Security = True";
+            }
+        }
+
+        public List<String> getFilm()
+        {
+            
+
+            return lf;
+        }
+
+        public String getGenre()
+        {
+
+
+            return "genre";
+        }
+
+        public String getActor()
+        {
+
+
+
+            return "actor";
+        }
+
+        public String getProducer()
+        {
+
+
+
+            return "producer";
+        }
+    }
+}
