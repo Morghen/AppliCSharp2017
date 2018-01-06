b7f62616327a501875a5e5ab389cdc32f0a86f6c
Morghen <remy.mauhin@gmail.com>
2018-01-06 17:36:33 +0100

    chose

--- AppCSharp2017/WebAtSmartVideo/Movies.aspx.cs
+++ AppCSharp2017/WebAtSmartVideo/Movies.aspx.cs
@@ -1,6 +1,6 @@
 ﻿using FilmsBLL;
 using FilmsDTO;
-using SmartWCFWindowService.ServiceReference;
+using SmartWCFService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,13 +12,12 @@ namespace WebAtSmartVideo
 {
     public partial class Movies : System.Web.UI.Page
     {
-        private SmartWcfClient _cli;
+        private SmartWcfClient _cli = new SmartWcfClient();
         private List<FilmDTO> _filmList;
 
         protected void Page_Load(object sender, EventArgs e)
         {
             //Connexion a la BDD et pull des 20 premiers films
-            _cli = new SmartWcfClient();
             _filmList = new List<FilmDTO>(_cli.getFilmList(0, 20));
             //Binding a la GridView nommée grid définie dans le fichier .cs
             grid.DataSource = _filmList;
